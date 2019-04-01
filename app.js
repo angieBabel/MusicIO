7a5ca118d859650d3fbd93a014241a55e86ffbcf
Angelica Barreda <angelica@tokyoplay.com>
2019-03-31 21:07:48 -0600

    Installs
    Install nom, nodeJs, express js and express-session

---
+++ app.js
@@ -0,0 +1,24 @@
+// Vue.component('movie-card', {
+//   props: ['image', 'title'],
+//   template: `
+//     <div>
+//       <img width="100" v-bind:src="image" v-bind:alt="title"/>
+//       <h2>{{ title }}</h2>  
+//     </div>
+//   `,
+// })
+
+// new Vue({
+//     el: '#app'
+// })
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
